16afa084c67f2103d8bc72a23484646d0317cd85
seb1441 <sebastienauriault@gmail.com>
2017-07-25 04:54:45 -0400

    Change environment

--- config/environment.rb
+++ config/environment.rb
@@ -3,7 +3,7 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
-
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   address: 'smtp.sendgrid.net',
   port: '587',
