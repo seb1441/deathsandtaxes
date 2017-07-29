d845e111c57a29cb66311c9575fda93b868eda47
seb1441 <sebastienauriault@gmail.com>
2017-07-29 04:28:48 -0400

    Fix Heroku

--- config/application.rb
+++ config/application.rb
@@ -10,6 +10,7 @@ module Frenzysquirrelz
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.1
+    config.assets.serve_static_files = true
 
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
