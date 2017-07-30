82e35f6896408dd6bb704ccc14f468103c165b1d
seb1441 <sebastienauriault@gmail.com>
2017-07-30 05:26:43 -0400

    Add more pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -7,4 +7,8 @@ class StaticPagesController < ApplicationController
     @users = User.all.order('character_name ASC')
     Time.zone = "Eastern Time (US & Canada)"
   end
+
+  def potion
+    @users = User.all.order('potion DESC')
+  end
 end
