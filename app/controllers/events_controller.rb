21ac5040bcc630eeb20f185e7beec89fbc92f036
seb1441 <sebastienauriault@gmail.com>
2017-08-02 05:28:30 -0400

    Remove access to events to Recruits and change button colors

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -2,6 +2,7 @@ class EventsController < ApplicationController
   before_action :set_event, only: [:show, :edit, :update, :destroy]
 
   before_action :check_authorized, :except => [:index, :addname, :removename]
+  before_action :check_recruit
 
   def addname
     event = Event.find(params[:id])
