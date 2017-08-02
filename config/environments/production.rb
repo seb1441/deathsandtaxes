9ddc6fa311fc91c86b70768bb6cda279fed17da8
seb1441 <sebastienauriault@gmail.com>
2017-08-02 15:38:06 -0400

    Change to FR production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -89,4 +89,5 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
   Rails.application.routes.default_url_options[:host] = 'deaths-and-taxes.herokuapp.com'
+  config.i18n.default_locale = :fr
 end
